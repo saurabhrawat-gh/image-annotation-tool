9c344c8b9e877319ea63bb34d6dd9eaaa7baf7e1
saurabh.rawat <saurabhrawat.official@gmail.com>
2025-03-27 05:43:55 +0530

    initial commit, cleanup code

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>
+        Hello you all beautiful people, let's create some magic with React!!
+        🚀🚀
+      </p>
+    </>
+  );
+}
+
+export default App;
